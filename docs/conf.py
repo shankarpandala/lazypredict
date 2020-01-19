43f41cb6cd31526bd174c864b10ee072c4a55710
Shankar Rao Pandala <shankar.pandala@gmail.com>
2020-01-19 23:17:35 +0530

    fix docs test2

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../lazypredict'))
 
-import lazypredict
+# import lazypredict
 
 # -- General configuration ---------------------------------------------
 

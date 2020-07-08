55b66646c0425f838edf37bfdfc75712299b2a03
Shankar Rao Pandala <shankarpandala@users.noreply.github.com>
2020-07-08 22:02:55 +0530

    Removed references to python version 2

--- setup.py
+++ setup.py
@@ -20,16 +20,12 @@ test_requirements = ['pytest>=3', ]
 setup(
     author="Shankar Rao Pandala",
     author_email='shankar.pandala@live.com',
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
+    python_requires='>=3.6, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',

abfba9bc606fe42ba400cab7896f3cf7719305be
Shankar Rao Pandala <shankar.pandala@gmail.com>
2020-07-25 14:22:53 +0530

    fix requirements file

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.rst") as readme_file:
 with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-with open("requirements.txt", "r") as fh:
+with open("requirements_dev.txt", "r") as fh:
     requirements = [line.strip() for line in fh]
 
 setup_requirements = [

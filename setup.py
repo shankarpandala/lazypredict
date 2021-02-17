e377953daeafa789d4f2a51703371f81849160f0
Shankar Rao Pandala <shankarpandala@users.noreply.github.com>
2021-02-17 21:05:46 +0530

    Bump version: 0.2.8 → 0.2.9

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/shankarpandala/lazypredict",
-    version='0.2.8',
+    version='0.2.9',
     zip_safe=False,
 )

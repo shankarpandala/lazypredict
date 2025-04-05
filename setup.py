688fc02298e2d9360457fa965219ea5643d1dba0
Shankar Pandala <shankar.pandala@gmail.com>
2025-04-06 00:58:38 +0530

    Bump version to 0.2.16 in .bumpversion.cfg, __init__.py, and setup.py

--- setup.py
+++ setup.py
@@ -51,6 +51,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/shankarpandala/lazypredict",
-    version='0.2.15',
+    version='0.2.16',
     zip_safe=False,
 )
